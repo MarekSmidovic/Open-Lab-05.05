9d72321c4404c05ff4abc305e1ed50f9f28663be
smidovic <msmidovic@gmail.com>
2021-11-28 12:51:32 +0100

    alternativne velke male pismena

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.
 
 namespace Open_Lab_05._05
 {
@@ -6,7 +7,69 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            
+            string vys = string.Empty;
+            int medzera = 0;
+
+            //zistenie pocet miest na vytvorenie arrayu
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == ' ')
+                {
+                    medzera += 1;
+                }
+            }
+            //zapisanie miest medzier
+            int[] cisloMedzery = new int[medzera];
+            int arrayUmiestnenie = 0;
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == ' ')
+                {
+                    cisloMedzery[arrayUmiestnenie] = i;
+                    arrayUmiestnenie++;
+                    
+                }
+            }
+
+            //vymazanie medzier zo stringu
+            string aBezMedzier = String.Empty;
+
+            foreach (char ch in original)
+            {
+                if (ch != ' ')
+                {
+                    aBezMedzier += ch;
+                }
+            }
+             
+            // zvacsovanie 
+            for (int i = 0; i <aBezMedzier.Length; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    char lenNaLoop = aBezMedzier[i];
+                    string velkePismeno = lenNaLoop.ToString().ToUpper();
+                    vys += velkePismeno;
+                }
+                else if (i % 2 == 1)
+                {
+                    char lenNaLoop = aBezMedzier[i];
+                    vys += lenNaLoop;
+                }
+                
+            }
+
+            //pridanie medzier naspat
+            for (int i = 0; i < medzera; i++)
+            {
+                vys = vys.Insert(cisloMedzery[i], " ")
+            }
+
+            return vys;
+            
         }
     }
 }
